ccc624b412d09ca8f40519eae9dbe5dc6e534461
Steven Hertz <StevenH@appi4.com>
2025-09-03 13:44:31 -0400

    Initial Commit

---
+++ Special children learning app/Special_children_learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Special_children_learning_appApp.swift
+//  Special children learning app
+//
+//  Created by Steven Hertz on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Special_children_learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
